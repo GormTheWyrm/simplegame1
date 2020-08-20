a27cf2c37a3ccff7833aef31b6569d02b9ad733e
Gorm <gdsoucie@gmail.com>
2020-08-19 20:07:41 -0400

    buttons now allow movement

--- src/components/Input/InputButtons.js
+++ src/components/Input/InputButtons.js
@@ -3,20 +3,38 @@ import './inputControls.css';
 
 
 function InputButtons(props) {
-  
-//may need to diplay other objects here...
-//such as player?
+
+  //may need to diplay other objects here...
+  //such as player?
 
 
   return (
     <div className="inputButtons">
-        <button onClick={props.handleLeftClick}>Left</button>
-        <button>Right</button>
-        <button>Up</button>
-        <button>Down</button>
-        <button>Test</button>
-        <button>Test</button>
-        <button onClick={props.testFunction}>Test</button>
+      <div className="btnRow">
+        {/* top div */}
+
+        <button className=" btn UpButton" onClick={props.handleUpClick}>Up</button>
+      </div>
+      <div className="btnRow">
+        {/* middle div */}
+        <button className="btn leftButton" onClick={props.handleLeftClick}>Left</button>
+        <button className="btn actionButton" onClick={props.testFunction}>Test</button>
+        <button className="btn rightButton" onClick={props.handleRightClick}>Right</button>
+
+      </div>
+      <div className="btnRow">
+        {/* bottom div */}
+
+        <button className="btn DownButton" onClick={props.handleDownClick}>Down</button>
+      </div>
+
+
+      <div className="btnRow">
+        {/* bottom div 2 */}
+        <button className="btn">Test</button>
+        <button className="btn" >Test</button>
+      </div>
+
     </div>
   );
 }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -7,8 +7,8 @@ class MainPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            playerLocX: 350,
-            playerLocY: 250
+            playerLocX: 150,
+            playerLocY: 150
             //increment these by 50!
 
 
@@ -33,8 +33,54 @@ class MainPage extends React.Component {
         else {
             console.log("cannot move left");
         }
-
-
+    }
+    handleClickLeft = () => {
+        console.log("click");
+        let oldX = this.state.playerLocX;
+        let newX = oldX - 50;
+        if (newX >= 0) {
+            this.setState({ playerLocX: newX })
+            console.log(this.state)
+        }
+        else {
+            console.log("cannot move left");
+        }
+    }
+    handleClickRight = () => {
+        console.log("click");
+        let oldX = this.state.playerLocX;
+        let newX = oldX + 50;
+        if (newX < 600) {
+            this.setState({ playerLocX: newX })
+            console.log(this.state)
+        }
+        else {
+            console.log("cannot move right");
+        }
+    }
+    handleClickUp = () => {
+        console.log("click");
+        let oldY = this.state.playerLocY;
+        let newY = oldY + 50;
+        if (newY < 600) {
+            this.setState({ playerLocY: newY })
+            console.log(this.state)
+        }
+        else {
+            console.log("cannot move Up");
+        }
+    }
+    handleClickDown = () => {
+        console.log("click");
+        let oldY = this.state.playerLocY;
+        let newY = oldY - 50;
+        if (newY >= 0) {
+            this.setState({ playerLocY: newY })
+            console.log(this.state)
+        }
+        else {
+            console.log("cannot move Up");
+        }
     }
 
 
@@ -45,13 +91,17 @@ class MainPage extends React.Component {
                     playerY={this.state.playerLocY} />
                 {/* insert input button and thne  text box so people on ipad can sort of play it */}
                 <div style={{ width: 600 + "px", border: "solid blue" }}>
-                    <InputButtons handleLeftClick={this.handleClickLeft} 
+                    <InputButtons 
+                        handleLeftClick={this.handleClickLeft} 
+                        handleRightClick={this.handleClickRight}
+                        handleUpClick={this.handleClickUp}
+                        handleDownClick={this.handleClickDown}
                     
                     />
                     <InputTextBox />
 
                 </div>
-                <button onClick={this.testFunction}>testFunction</button>
+                
             </div>
         );
     }
